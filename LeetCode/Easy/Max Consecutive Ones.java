3d5e328692f799e99e62fa50de51589905aeac8d
K T Narasimha Naidu <141550407+naidu199@users.noreply.github.com>
2024-08-26 21:56:46 +0530

    Create Max Consecutive Ones.java

---
+++ LeetCode/Easy/Max Consecutive Ones.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int c=0;
+        int max=0;
+        for(int i : nums){
+            if(i==1){
+                c++;
+                if(c>max){
+                    max=c;
+                }
+            }else{
+                
+                c=0;
+            }
+        }
+        return max;
+    }
+}
