86654d1045cee1b06f81fb9d8988d401aac87e2b
Narasimha Naidu Korrapati <141550407+naidu199@users.noreply.github.com>
2025-05-17 08:02:15 +0530

    Update Sort Colors.java

--- LeetCode/Medium/Sort Colors.java
+++ LeetCode/Medium/Sort Colors.java
@@ -1,3 +1,32 @@
+//optimal
+class Solution {
+    public void sortColors(int[] nums) {
+        int l=0,mid=0,r=nums.length-1;
+
+        while(mid<=r){
+            if(nums[mid]==0){
+                swap(nums,l,mid);
+                l++;
+                mid++;
+            
+            }else if(nums[mid]==1){
+                mid++;
+            }else if(nums[mid]==2){
+                swap(nums,mid,r);
+                r--;
+            }
+        }
+    }
+
+    public void swap(int[] arr,int i,int j){
+        int t=arr[i];
+        arr[i]=arr[j];
+        arr[j]=t;
+    }
+}
+
+//OR
+
 class Solution {
     public void sortColors(int[] nums) {
         int n=nums.length;
