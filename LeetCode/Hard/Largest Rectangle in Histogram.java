5a956364a602963c4a91afb88a87fd3c83e0af01
Narasimha Naidu Korrapati <141550407+naidu199@users.noreply.github.com>
2025-05-19 20:17:41 +0530

    Create Largest Rectangle in Histogram.java

---
+++ LeetCode/Hard/Largest Rectangle in Histogram.java
@@ -0,0 +1,74 @@
+
+Complexity
+Time	O(n)
+Space	O(n)
+
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int ans=0;
+        int n=heights.length;
+        int[] nse=nextSE(heights);
+        int[] pse=prevSE(heights);
+        for(int i=0;i<n;i++){
+            int a=heights[i]*(nse[i]-pse[i]-1);
+            ans=Math.max(a,ans);
+        }
+        return ans;
+    }
+    public int[] nextSE(int[] arr){
+        int n=arr.length;
+        int[] nse=new int[n];
+        Stack<Integer> stack=new Stack<>();
+
+        for(int i=n-1;i>=0;i--){
+            while(!stack.isEmpty() && arr[stack.peek()]>arr[i]){
+                stack.pop();
+            }
+            nse[i]=stack.isEmpty()?n:stack.peek();
+            stack.push(i);
+        }
+        return nse;
+    }
+
+    public int[] prevSE(int[] arr){
+        int n=arr.length;
+        int[] pse=new int[n];
+        Stack<Integer> stack=new Stack<>();
+
+        for(int i=0;i<n;i++){
+            while(!stack.isEmpty() && arr[stack.peek()]>=arr[i]){
+                stack.pop();
+            }
+            pse[i]=stack.isEmpty()?-1:stack.peek();
+            stack.push(i);
+        }
+        return pse;
+    }
+}
+
+Complexity
+Time	O(n^2)
+Space	O(1)
+
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int ans=0;
+        int n=heights.length;
+        if(n==1){
+            return heights[0];
+        }
+        for(int i=0;i<n;i++){
+            int max=heights[i];
+            int min=heights[i];
+            for(int j=i+1;j<n;j++ ){
+                min=Math.min(min,heights[j]);
+                int a=(j-i+1)*min;
+                max=Math.max(max,a);
+            }
+            if(max>ans){
+                ans=max;
+            }
+        }
+        return ans;
+    }
+}
