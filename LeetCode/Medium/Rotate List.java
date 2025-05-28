b13d33b17745528bdb0ab53c0c6f18de01529151
Narasimha Naidu Korrapati <141550407+naidu199@users.noreply.github.com>
2025-05-28 19:48:27 +0530

    Update Rotate List.java

--- LeetCode/Medium/Rotate List.java
+++ LeetCode/Medium/Rotate List.java
@@ -10,32 +10,30 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(head==null||head.next==null||k==0){
+       if(head==null||head.next==null||k==0){
             return head;
         }
         int len=0;
         ListNode ptr = head;
-        ListNode temp1=head;
-        
+        ListNode temp=head;
         while(ptr.next!=null){
             len++;
             ptr=ptr.next;
         }
         len++;
-       
-        if(k>len){
-            k=k%len;
-        }
-         if(k==len||k==0){
+        k=k%len;
+        if(k==len||k==0){
             return head;
         }
+
         for(int i=0;i<len-k-1;i++){
-            temp1=temp1.next;
+            temp=temp.next;
         }
-        ListNode temp2=temp1.next;
-        temp1.next=null;
+        ListNode temp1=temp.next;
+        temp.next=null;
         ptr.next=head;
-        return temp2;
+
+        return temp1;
 
     }
 }
