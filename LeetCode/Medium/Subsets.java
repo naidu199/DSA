52cde1af67c751f74d2d111b05ac21c751db3fa7
Narasimha Naidu Korrapati <141550407+naidu199@users.noreply.github.com>
2025-07-25 15:24:20 +0530

    Update Subsets.java

--- LeetCode/Medium/Subsets.java
+++ LeetCode/Medium/Subsets.java
@@ -1,3 +1,4 @@
+//O(2^n * n)
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         int n=nums.length;
@@ -14,3 +15,28 @@ class Solution {
         return ans;
     }
 }
+
+//O(2^N)
+class Solution {
+    private ArrayList<List<Integer>> list;
+    public List<List<Integer>> subsets(int[] nums) {
+        int n=nums.length;
+
+        list = new ArrayList<>();
+        subSets(0,nums,new ArrayList<>());
+        return list;
+    }
+
+    public void subSets(int i,int[] nums,ArrayList<Integer> sub){
+        if(i==nums.length){
+            list.add(new ArrayList<>(sub));
+            return;
+        }
+        subSets(i+1,nums,sub);
+        sub.add(nums[i]);
+        subSets(i+1,nums,sub);
+        sub.remove(sub.size() - 1);
+
+    }
+}
+
