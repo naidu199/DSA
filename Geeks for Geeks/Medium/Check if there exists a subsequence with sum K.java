a035594d44f6be5b1ed9f8b0f01b93931ce8292e
Narasimha Naidu Korrapati <141550407+naidu199@users.noreply.github.com>
2025-07-25 15:49:22 +0530

    Update Check if there exists a subsequence with sum K.java

--- Geeks for Geeks/Medium/Check if there exists a subsequence with sum K.java
+++ Geeks for Geeks/Medium/Check if there exists a subsequence with sum K.java
@@ -1,5 +1,5 @@
 // User function Template for Java
-
+//O(2^n)
 class Solution {
     public static boolean checkSubsequenceSum(int n, int[] arr, int k) {
         // code here
@@ -21,3 +21,29 @@ class Solution {
     }
     
 }
+
+//O(n*k)
+//recursion + memo
+class Solution {
+    public static boolean checkSubsequenceSum(int n, int[] arr, int k) {
+        Boolean[][] dp = new Boolean[n + 1][k + 1];
+        return subP(n, k, arr, dp);
+    }
+
+    private static boolean subP(int i, int target, int[] arr, Boolean[][] dp) {
+        if (target == 0) return true;
+        if (i == 0) return false;
+
+        if (dp[i][target] != null) return dp[i][target];
+
+        if (arr[i - 1] > target) {
+            return dp[i][target] = subP(i - 1, target, arr, dp);
+        }
+
+        boolean take = subP(i - 1, target - arr[i - 1], arr, dp);
+        boolean notTake = subP(i - 1, target, arr, dp);
+
+        return dp[i][target] = take || notTake;
+    }
+}
+
