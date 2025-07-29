4212f6cdccb2206cf6e53405abe4a16cb77f1ff1
Narasimha Naidu Korrapati <141550407+naidu199@users.noreply.github.com>
2025-07-29 21:42:20 +0530

    Update Binary Tree Inorder Traversal.java

--- LeetCode/Easy/Binary Tree Inorder Traversal.java
+++ LeetCode/Easy/Binary Tree Inorder Traversal.java
@@ -20,6 +20,20 @@ class Solution {
         return list;
     }
 }
+// recursion
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        inorder(root,list);
+        return list;
+    }
+    public void inorder(TreeNode node,List<Integer> list){
+        if(node==null) return ;
+        inorder(node.left,list);
+        list.add(node.val);
+        inorder(node.right,list);
+    }
+}
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
