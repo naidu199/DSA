f9c7dd156eb78321d5813350b0e1bba440377cdc
Narasimha Naidu Korrapati <141550407+naidu199@users.noreply.github.com>
2025-08-05 07:35:19 +0530

    Update Lowest Common Ancestor of a Binary Tree.java

--- LeetCode/Medium/Lowest Common Ancestor of a Binary Tree.java
+++ LeetCode/Medium/Lowest Common Ancestor of a Binary Tree.java
@@ -7,6 +7,24 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+// TC O(N)  SC O(log N)
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null||p==root||q==root) return root;
+
+        TreeNode left=lowestCommonAncestor(root.left,p,q);
+        TreeNode right=lowestCommonAncestor(root.right,p,q);
+
+        if(left==null){
+            return right;
+        }else if(right==null){
+            return left;
+        }
+        return root;
+    }
+}
+
+//TC O(N) && SC O(N)
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         ArrayList<TreeNode> l1=new ArrayList<>();
