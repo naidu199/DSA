dac52faecfe253d57d8835a4aef77a8ba4657f78
Narasimha Naidu Korrapati <141550407+naidu199@users.noreply.github.com>
2025-08-29 07:33:33 +0530

    Update Number of Islands.java

--- LeetCode/Medium/Number of Islands.java
+++ LeetCode/Medium/Number of Islands.java
@@ -50,3 +50,46 @@ class Solution {
         
     }
 }
+
+
+//DFS
+
+class Solution {
+    public int numIslands(char[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
+
+        int[][] visited=new int[n][m];
+
+        int count=0;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(visited[i][j]!=1 && grid[i][j]=='1'){
+                    dfs(i,j,grid,visited,n,m);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+
+    public void dfs(int r,int c,char[][] grid,int[][] visited ,int n,int m){
+
+        if (r < 0 || r >= n || c < 0 || c >= m || grid[r][c] == '0' || 
+        visited[r][c] == 1) {
+            return;
+        }
+        visited[r][c] = 1;
+        int[] dr = {-1, 0, 1, 0};
+        int[] dc = {0, 1, 0, -1};
+        for(int i=0;i<4;i++){
+            dfs(r+dr[i],c+dc[i],grid,visited,n,m);
+        }
+        
+        
+        
+    }
+}
